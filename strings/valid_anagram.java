63c9bc1bf3b350952367601e743b30abc276d292
radhamankar <radhamankar50@gmail.com>
2026-03-23 09:20:20 +0530

    Day 3 valid anagram

---
+++ strings/valid_anagram.java
@@ -0,0 +1,21 @@
+package strings;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+         if(n!=m)return false;
+          int[] freq=new int[26]; 
+        
+        for(int i=0;i<n;i++){
+        
+        freq[s.charAt(i)-'a']++;
+        freq[t.charAt(i)-'a']--;
+        }
+
+        for(int it:freq){
+            if(it!=0)return false;
+        }
+        return true;
+        
+    }
+}
