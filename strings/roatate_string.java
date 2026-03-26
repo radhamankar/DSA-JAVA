4d78759c3828500b1af56c323c48627b9036f317
radhamankar <radhamankar50@gmail.com>
2026-03-26 22:48:30 +0530

    rotate string

---
+++ strings/roatate_string.java
@@ -0,0 +1,13 @@
+package strings;
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.length()!=goal.length())return false;
+
+        String result=s+s;
+
+        return result.contains(goal);
+        
+    }
+}
