4142912507cc0eddd425677a44918fd1a38165bd
radhamankar <radhamankar50@gmail.com>
2026-06-06 23:22:36 +0530

    Move Zeros

--- strings/isomorphic_strings.java
+++ strings/isomorphic_strings.java
@@ -1,6 +1,6 @@
 package strings;
 import java.util.HashMap;
-class isomorphic_strings {
+class Solution {
     public boolean isIsomorphic(String s, String t) {
         int n=s.length();
         HashMap<Character,Character> map1=new HashMap<>();
